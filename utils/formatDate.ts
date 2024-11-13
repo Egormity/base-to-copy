781d82fcb08988f00cd6543aea1cbbca3a911075
Egormity <kotlaregor9@gmail.com>
2024-11-13 22:01:41 +0700

    some changes

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,9 +1,10 @@
-export function formatDate(date: Date | string, type?: 'digit' | 'text') {
-  let options = {};
-  if (type === 'digit') options = { day: '2-digit', month: '2-digit', year: 'numeric' };
-  if (type === 'text') options = { month: 'long', day: 'numeric', year: 'numeric' };
+export function formatDate(date: Date | string, type?: "digit" | "text") {
+    let options = {};
 
-  const dateToFormat = typeof date === 'string' ? new Date(date) : date;
+    if (type === "digit") options = { day: "2-digit", month: "2-digit", year: "numeric" };
+    if (type === "text") options = { month: "long", day: "numeric", year: "numeric" };
 
-  return new Intl.DateTimeFormat(undefined, options).format(dateToFormat);
+    const dateToFormat = typeof date === "string" ? new Date(date) : date;
+
+    return new Intl.DateTimeFormat(undefined, options).format(dateToFormat);
 }
